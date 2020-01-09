1fd673f4be4c830543f077d520f3b7392f211580
Rebecca Henderson <RebeccaH29@users.noreply.github.com>
2020-01-09 09:09:29 +0000

    tidy code

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,24 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { ProductConsumer } from '../context/context';
-import { FaCartPlus } from 'react-icons/fa'
 import { Link } from 'react-router-dom';
 
-
-/*const products = ([
-    { name: "Travel Book", price: 12.49, tax: 0, quantity: 0 },
-    { name: "Music CD", price: 14.99, tax: 0.20, quantity: 0 },
-    { name: "Chocolate bar", price: 0.85, tax: 0, quantity: 0 },
-    { name: "Imported box of chocolates", price: 10, tax: 0.05, quantity: 2 },
-    { name: "Imported bottle of perfume", price: 47.50, tax: 0.25, quantity: 0 },
-    { name: "Bottle of perfume", price: 18.99, tax: 0.20, quantity: 0 },
-    { name: "Packet of painkillers", price: 9.75, tax: 0, quantity: 0 }
-  ]); */
-
 export default function Product({product}) {
-//class Product extends Component {
-   
-    //render() {
  
     return (<div>
         <ProductConsumer>
